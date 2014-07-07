8778a45e3999d4a283c2e5fbb62cb55c7ffddcbd
Erik Hollensbe <github@hollensbe.org>
2014-07-06 17:45:26 -0700

    three more benchmarks that send data over the pipe

--- spdy_bench_test.go
+++ spdy_bench_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"sync"
 	"testing"
 )
@@ -67,3 +68,50 @@ func BenchmarkDialWithSPDYStream10000(b *testing.B) {
 		}
 	}
 }
+
+func benchmarkStreamWithDataAndSize(size uint64, b *testing.B) {
+	server, wg := configureServer()
+
+	defer func() {
+		server.Close()
+		wg.Wait()
+	}()
+
+	for i := 0; i < b.N; i++ {
+		conn, dialErr := net.Dial("tcp", LISTEN_ADDRESS)
+		if dialErr != nil {
+			b.Fatalf("Error dialing server: %s", dialErr)
+		}
+
+		spdyConn, spdyErr := NewConnection(conn, false)
+		if spdyErr != nil {
+			b.Fatalf("Error creating spdy connection: %s", spdyErr)
+		}
+
+		go spdyConn.Serve(MirrorStreamHandler)
+
+		stream, err := spdyConn.CreateStream(http.Header{}, nil, false)
+
+		writer := make([]byte, size)
+
+		stream.Write(writer)
+
+		if err != nil {
+			panic(err)
+		}
+
+		reader := make([]byte, size)
+		stream.Read(reader)
+
+		stream.Close()
+
+		closeErr := spdyConn.Close()
+		if closeErr != nil {
+			b.Fatalf("Error closing connection: %s, closeErr")
+		}
+	}
+}
+
+func BenchmarkStreamWith1Byte10000(b *testing.B)     { benchmarkStreamWithDataAndSize(1, b) }
+func BenchmarkStreamWith1KiloByte10000(b *testing.B) { benchmarkStreamWithDataAndSize(1024, b) }
+func BenchmarkStreamWith1Megabyte10000(b *testing.B) { benchmarkStreamWithDataAndSize(1024*1024, b) }
